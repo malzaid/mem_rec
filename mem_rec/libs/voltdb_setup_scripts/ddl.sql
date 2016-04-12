c9fc28a9e8f9ce1032f004964df7f750891d392f
Azamat Kamzin <akamzin@gmail.com>
2016-04-11 17:24:16 -0700

    Voltdb--

--- mem_rec/libs/voltdb_setup_scripts/ddl.sql
+++ mem_rec/libs/voltdb_setup_scripts/ddl.sql
@@ -28,20 +28,4 @@ CREATE TABLE movies (
 
 
 END_OF_BATCH
--- 
--- -- Update classes from jar so that the server will know about classes
--- -- but not procedures yet.
--- -- This command cannot be part of a DDL batch.
--- LOAD CLASSES voter-procs.jar;
--- 
--- -- The following CREATE PROCEDURE statements can all be batched.
--- file -inlinebatch END_OF_2ND_BATCH
--- 
--- -- stored procedures
--- CREATE PROCEDURE FROM CLASS voter.Initialize;
--- CREATE PROCEDURE FROM CLASS voter.Results;
--- CREATE PROCEDURE PARTITION ON TABLE votes COLUMN phone_number FROM CLASS voter.Vote;
--- CREATE PROCEDURE FROM CLASS voter.ContestantWinningStates;
--- CREATE PROCEDURE FROM CLASS voter.GetStateHeatmap;
--- 
--- END_OF_2ND_BATCH
+
