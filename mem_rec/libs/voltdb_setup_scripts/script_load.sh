6cc9e62d5a9a92a6438b0a5180cfcaa0254ebf44
Azamat Kamzin <akamzin@gmail.com>
2016-04-15 21:19:05 -0700

    automating voltdb start

--- mem_rec/libs/voltdb_setup_scripts/script_load.sh
+++ mem_rec/libs/voltdb_setup_scripts/script_load.sh
@@ -1,14 +1,6 @@
+#!/bin/bash
+
 #compile java stored procedures
-
-rm procedures/mem_rec/*.class
-javac -cp "$CLASSPATH:/home/akamzin1/voltdb-ent-6.1/voltdb/*" procedures/mem_rec/*.java
-jar cvf storedprocs.jar procedures/mem_rec/*.class
-
-
-
-sqlcmd < ddl.sql
-csvloader --separator "," --file movies.csv movies
-csvloader --separator "," --file ratings.csv ratings
 
 
 cd procedures/mem_rec/
@@ -17,3 +9,6 @@ javac -cp "$CLASSPATH:/home/akamzin1/voltdb-ent-6.1/voltdb/*" *.java
 jar cvf storedprocs.jar *.class
 mv storedprocs.jar ../../.
 cd ../..
+sqlcmd < ddl.sql
+csvloader --separator "," --file movies.csv movies
+csvloader --separator "," --file ratings.csv ratings
