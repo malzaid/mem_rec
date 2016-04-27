e8abc9097507f49a1ac16081f22569eae2eb8850
Azamat Kamzin <akamzin@gmail.com>
2016-04-26 21:18:31 -0700

    fixing old dataset

---
+++ mem_rec/libs/voltdb_setup_scripts/ddl2.sql
@@ -0,0 +1,46 @@
+
+-- Tell sqlcmd to batch the following commands together,
+-- so that the schema loads quickly.
+file -inlinebatch END_OF_BATCH
+
+
+-- ----------------------------
+--  Table structure for "ratings"
+-- ----------------------------
+
+CREATE TABLE ratings2 (
+	userid		integer		NOT NULL,
+	movieid		integer		NOT NULL,
+	rating		FLOAT		NOT NULL,
+	timestamp	integer		NOT NULL
+);
+
+-- ----------------------------
+--  Table structure for "movies"
+-- ----------------------------
+
+CREATE TABLE movies2 (
+	movieid	integer			NOT NULL,
+	title	varchar(255)	NOT NULL,
+	genres	varchar(255)	NOT NULL
+)
+
+
+
+
+END_OF_BATCH
+
+CREATE INDEX movies_idx ON movies2 (movieid);
+CREATE INDEX ratings_idx ON ratings2 (userid,movieid);
+
+-- LOAD CLASSES storedprocs.jar;
+
+--
+-- CREATE PROCEDURE FROM CLASS PrepareUsers;
+-- CREATE PROCEDURE FROM CLASS PrepareItems;
+-- CREATE PROCEDURE FROM CLASS PrepareEvents;
+-- CREATE PROCEDURE FROM CLASS PrepareUserEvents;
+-- CREATE PROCEDURE FROM CLASS PrepareItemEvents;
+-- CREATE PROCEDURE FROM CLASS PrepareItemUsers;
+
+
